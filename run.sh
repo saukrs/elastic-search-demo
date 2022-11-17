3437780c95d4e453805f2ec1430e577befecd9c2
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-17 20:11:18 +0200

    run.sh: Indent the function code

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 function set_index_replicas () {
-curl -XPUT 'localhost:9200/sampleindex/_settings?pretty' \
-  -H 'Content-Type: application/json; charset=utf-8' \
-  -d @- << \
+    curl -XPUT 'localhost:9200/sampleindex/_settings?pretty' \
+      -H 'Content-Type: application/json; charset=utf-8' \
+      -d @- << \
 ----------------------------------------------------------
 {
   "settings": {
