01c9621a64d9d2c718c4c69492e36b2aa2096d09
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-17 20:22:50 +0200

    run.sh: Make function Bourne-alike

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-function set_index_replicas () {
+set_index_replicas()
+{
     curl -XPUT 'localhost:9200/sampleindex/_settings?pretty' \
       -H 'Content-Type: application/json; charset=utf-8' \
       -d @- <<- \
