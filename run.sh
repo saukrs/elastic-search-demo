c118c8ba2751e771b84a9dbbcee0d38a303cf659
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-17 19:34:08 +0200

    run.sh: Import temp. `change-number_of_replicas.sh` script

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env sh
+
+curl -XPUT 'localhost:9200/sampleindex/_settings?pretty' \
+  -H 'Content-Type: application/json; charset=utf-8' \
+  -d @- << \
+----------------------------------------------------------
+{
+  "settings": {
+    "index": {
+      "number_of_replicas": 2
+    }
+  }
+}
+----------------------------------------------------------
