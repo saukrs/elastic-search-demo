b11e8e3b2d16dac6c9837b9709658a1640b2ad43
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-18 20:04:11 +0200

    demo.sh: Make `read`s Bourne-shell compatible

--- demo.sh
+++ demo.sh
@@ -6,34 +6,34 @@ echo; echo - Initial state
 echo; elrun list_cluster_settings
 echo; elrun list_disk
 echo; elrun list_indices
-echo; echo Next?; read
+echo; echo Next?; read REPLY
 
 echo; echo - Exclude the node
 echo; elrun exclude_node es03
 echo; elrun list_disk
 echo; elrun list_indices
-echo; echo Next?; read
+echo; echo Next?; read REPLY
 
 echo; echo - Shutdown the node
 echo; elrun cluster_es03_down
 echo; elrun cluster_es03_offsite_down
 echo; elrun list_disk
 echo; elrun list_indices
-echo; echo Next?; read
+echo; echo Next?; read REPLY
 
 echo; echo - Bring up the backup node
 echo; elrun cluster_es03_offsite_up
 echo; elrun list_nodes
 echo; elrun list_disk
 echo; elrun list_indices
-echo; echo Next?; read
+echo; echo Next?; read REPLY
 
 echo; echo - Wait for the node to joing the cluster
 echo; sleep 5
 echo; elrun list_nodes
 echo; elrun list_disk
 echo; elrun list_indices
-echo; echo Next?; read
+echo; echo Next?; read REPLY
 
 echo; echo - Allow node to be routed again
 echo; elrun exclude_node
