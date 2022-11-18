f3a7482952c593853316eb84e0d491e35babc4b9
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-18 18:47:54 +0200

    demo.sh: Split cmds into groups

--- demo.sh
+++ demo.sh
@@ -1,19 +1,25 @@
 elrun list_cluster_settings
 elrun list_disk
 elrun list_indices
+
 elrun exclude_node es03
 elrun list_disk
 elrun list_indices
+
 elrun cluster_es03_down
 elrun list_disk
 elrun list_indices
+
 elrun cluster_es03_offsite_up
 elrun list_nodes
 elrun list_disk
 elrun list_indices
+
+sleep 5
 elrun list_nodes
 elrun list_disk
 elrun list_indices
+
 elrun exclude_node
 elrun list_disk
 elrun list_indices
