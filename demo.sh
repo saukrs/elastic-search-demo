55b32225bd6d23eb45a01aa6b65af2ac9aba5892
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-19 00:06:23 +0200

    demo.sh: TODOs to implement soon

--- demo.sh
+++ demo.sh
@@ -8,6 +8,12 @@ kill_after()
 
 clear
 
+echo; echo - TODO: Refactor printing step names + asking a confirmation to continue \(after the step name is announced\) into a fn:
+
+# step "- Starting main site:" ask
+
+echo; echo - TODO: Building containers for two sites:
+
 echo; echo - Starting main site:
 echo; elrun cluster__up
 echo; echo Next?; read REPLY
@@ -23,6 +29,16 @@ echo; elrun list_disk
 echo; elrun list_indices
 echo; elrun list_shards
 echo; echo Next?; read REPLY
+
+echo; echo - TODO: Creating ES indices:
+
+echo; echo - TODO: Changing ES replicas number:
+
+# health status index        uuid                   pri rep docs.count docs.deleted store.size pri.store.size
+# green  open   sampleindex2 OoN6uwOdS9ueoWw1Zuw1Xg   1   1          0            0       416b           208b
+# yellow open   sampleindex  vcsIbSLYSzu7YNtBfNXUNg   1   2          0            0       416b           208b
+# yellow open   hehe-index   WNaN5TZTQ92Vr81lgJQQlQ   1   3          0            0       416b           208b
+# yellow open   laba-diena   nGeFJqc3QrKjVmpeL1XvzA   1   4          0            0       416b           208b
 
 echo; echo - Exclude the node
 echo; elrun exclude_node es03
@@ -77,5 +93,7 @@ echo; echo Next?; read REPLY
 echo; echo - Destroy the containers:
 echo; elrun cluster_es03_offsite_down
 echo; elrun cluster__down
+
+echo; echo - TODO: Cleanup the used Docker Compose volumes too \(a change in \`elrun\`\).
 
 echo; echo - Demo is over.
