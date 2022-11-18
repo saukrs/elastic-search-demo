26e2c32bf1d1852837356f37e0c9a6ad46f4a010
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-18 18:47:37 +0200

    demo.sh: Import cmds from Bash history

---
+++ demo.sh
@@ -0,0 +1,19 @@
+elrun list_cluster_settings
+elrun list_disk
+elrun list_indices
+elrun exclude_node es03
+elrun list_disk
+elrun list_indices
+elrun cluster_es03_down
+elrun list_disk
+elrun list_indices
+elrun cluster_es03_offsite_up
+elrun list_nodes
+elrun list_disk
+elrun list_indices
+elrun list_nodes
+elrun list_disk
+elrun list_indices
+elrun exclude_node
+elrun list_disk
+elrun list_indices
